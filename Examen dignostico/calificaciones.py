b9f5fa354309db6dfeaf5086af67a8580b34f914
MiguelN23590272 <l23590272@sjuanrio.tecnm.mx>
2024-08-23 13:43:39 -1100

    EXAMEN DIAGNOSTICO

---
+++ Examen dignostico/calificaciones.py
@@ -0,0 +1,26 @@
+
+n= int(input("Introduce el numero de alumnos: "))
+
+rep=0
+for i in range (n):
+	nombre=input("Ingresa el nombre del alumno: "), {i+1}
+	cal=[]
+	
+	for k in range (3):
+	cal=input("Ingresa las calificaciones")
+	
+	scal=0
+	
+	for k in range(3):
+		cal=float(input("Ingresa las calificaciones: "), {k+1}
+		scal +=cal
+		
+	prom= scal/3
+	
+	if promedio < 70:
+		print({nombre},"reprobado: ")
+	else:
+		print({nombre},"Su promedio es de: ")
+	
+	
+
